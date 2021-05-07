52c5da7066a49c317ef643c47506d161e6f32dd9
Caldenet <dizeoc@gmail.com>
2021-05-07 17:54:57 -0300

    664

--- inc/layout/header.php
+++ inc/layout/header.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -8,7 +9,9 @@
     <link rel="stylesheet" href="https://necolas.github.io/normalize.css/8.0.0/normalize.css">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.4.1/css/all.css" integrity="sha384-5sAR7xN1Nv6T6+dT2mhtzEpVJvfS3NScPQTrOxhwjIuvcA67KV2R5Jz6kr4abQsz" crossorigin="anonymous">
     <link rel="preconnect" href="https://fonts.gstatic.com">
-<link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@100&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@100&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="css/style.css">
 </head>
+
 <body>
--- index.php
+++ index.php
@@ -23,10 +23,60 @@
         <div class="campo enviar">
             <input type="submit" value="Añadir">
         </div>
-
     </form>
 </div>
 
+<div class="bg-blanco contenedor sombra contactos">
+    <div class="contenedor-contactos">
+        <h2>Contactos</h2>
 
+        <input type="text" id="buscar" class="buscador sombra" placeholder="Buscar contactos">
+        <p class="total-contactos"><span>2</span> contactos</p>
+        <div class="contenedor-tabla">
+            <table id="listado-contactos" class="listado-contactos">
+                <thead>
+                    <tr>
+                        <th>Nombre</th>
+                        <th>Empresa</th>
+                        <th>Telefono</th>
+                        <th>Acciones</th>
+                    </tr>
+                <tbody>
+                    <tr>
+                        <td>Juan</td>
+                        <td>Udemy</td>
+                        <td>021548754</td>
+                        <td><a href="#" class="btn-editar btn">
+                                <i class="fas fa-pen-square"></i>
+                            </a>
+                            <button data-id="1" type="button" class="btn-borrar btn"><i class="fas fa-trash-alt"></i></button>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Juan</td>
+                        <td>Udemy</td>
+                        <td>021548754</td>
+                        <td><a href="#" class="btn-editar btn">
+                                <i class="fas fa-pen-square"></i>
+                            </a>
+                            <button data-id="1" type="button" class="btn-borrar btn"><i class="fas fa-trash-alt"></i></button>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Juan</td>
+                        <td>Udemy</td>
+                        <td>021548754</td>
+                        <td><a href="#" class="btn-editar btn">
+                                <i class="fas fa-pen-square"></i>
+                            </a>
+                            <button data-id="1" type="button" class="btn-borrar btn"><i class="fas fa-trash-alt"></i></button>
+                        </td>
+                    </tr>
+                </tbody>
+                </thead>
+            </table>
+        </div>
+    </div>
+</div>
 
 <?php include 'inc/layout/footer.php' ?>
