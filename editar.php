c4b8bae67fc509ab7a625d1d55b5ee8b4c598e2d
caldenet <dizeoc@gmail.com>
2021-05-10 20:13:20 -0300

    665

---
+++ editar.php
@@ -0,0 +1,15 @@
+<?php include 'inc/layout/header.php' ?>
+
+<div class="contenedor-barra">
+    <div class="contenedor barra">
+        <a href="index.php" class="btn volver">Volver</a>
+        <h1>Editar contacto</h1>
+    </div>
+</div>
+<div class="bg-amarillo contenedor sombra">
+    <form action="#" id="contacto">
+        <legend>Editar contacto <span>Todos los campos son obligatorios</span></legend>
+        <?php include 'inc/layout/formulario.php' ?>
+    </form>
+</div>
+<?php include 'inc/layout/footer.php' ?>
--- index.php
+++ index.php
@@ -6,23 +6,7 @@
 <div class="bg-amarillo contenedor sombra">
     <form action="#" id="contacto">
         <legend>Añada un contacto <span>Todos los campos son obligatorios</span></legend>
-        <div class="campos">
-            <div class="campo">
-                <label for="nombre">Nombre:</label>
-                <input type="text" placeholder="Nombre de contacto" id="nombre">
-            </div>
-            <div class="campo">
-                <label for="empresa">Empresa:</label>
-                <input type="text" placeholder="Nombre de la empresa" id="empresa">
-            </div>
-            <div class="campo">
-                <label for="telefono">Telefono:</label>
-                <input type="text" placeholder="Telefono de contacto" id="telefono">
-            </div>
-        </div>
-        <div class="campo enviar">
-            <input type="submit" value="Añadir">
-        </div>
+        <?php include 'inc/layout/formulario.php' ?>
     </form>
 </div>
 
@@ -46,7 +30,7 @@
                         <td>Juan</td>
                         <td>Udemy</td>
                         <td>021548754</td>
-                        <td><a href="#" class="btn-editar btn">
+                        <td><a href="editar.php?id=1" class="btn-editar btn">
                                 <i class="fas fa-pen-square"></i>
                             </a>
                             <button data-id="1" type="button" class="btn-borrar btn"><i class="fas fa-trash-alt"></i></button>
