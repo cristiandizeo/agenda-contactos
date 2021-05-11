ec18cba25370086f41d8d2b5e65e9653dbdc4f42
Caldenet <dizeoc@gmail.com>
2021-05-11 17:37:51 -0300

    668

--- js/app.js
+++ js/app.js
@@ -11,14 +11,48 @@ function leerFormulario(e) {
   //Leer los datos de los inputs
   const nombre = document.querySelector("#nombre").value,
     empresa = document.querySelector("#empresa").value,
-    telefono = document.querySelector("#telefono").value;
+    telefono = document.querySelector("#telefono").value,
+    accion =document.querySelector('#accion').value;
 
   if (nombre === "" || empresa === "" || telefono === "") {
     //parametros texto y clase
     mostrarNotificacion("Todos los campos son obligatorios", "error");
   } else {
-    console.log(nombre, empresa, telefono);
-  }
+      //pasa la validacion crear llamado a ajax
+    const infoContacto = new FormData();
+    infoContacto.append('nombre', nombre);
+    infoContacto.append('empresa', empresa);
+    infoContacto.append('telefono', telefono);
+    infoContacto.append('accion', accion);
+
+    console.log(...infoContacto);
+
+    if(accion === 'crear'){
+        insertarBD(infoContacto);
+    }else{
+        
+    }
+    }
+}
+//inserta en bdd por ajax
+function insertarBD(datos){
+//llamado a ajax
+const xhr = new XMLHttpRequest();
+//crear objeto
+xhr.open('POST', 'inc/modelos/modelo-contactos.php', true);
+//abrir conexion
+xhr.onload = function(){
+    if(this.status === 200){
+        console.log (JSON.parse(xhr.responseText));
+        //lee respuesta php
+        const respuesta = JSON.parse(xhr.responseText);
+        console.log(respuesta.empresa);
+    }
+}
+//pasar datos
+
+//enviar datos
+xhr.send(datos);
 }
 
 //notificacion
